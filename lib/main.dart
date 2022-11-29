f5a88e912d9598dab312960d65d2a8a8f6b4755e
김봉연 <bykim@angeluce.kr>
2022-11-29 21:43:03 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Planter',
+      theme: ThemeData(
+        // 상호작용 요소에 사용되는 색상
+        brightness: Brightness.light,
+
+        // 앱의 주요부분 배경 색 (앱바, 탭바 등)
+        primaryColor: Colors.greenAccent,
+
+        // 위젯의 전경색
+        accentColor: Colors.redAccent,
+
+        // 앱에 기본으로 사용될 폰트
+        // fontFamily: 'IBM-Sans'
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Calendar2(key: UniqueKey()),
+          // child: Text('hello 채윤!'),
+        ),
+      ),
+    );
+  }
+}
+
+class Calendar2 extends StatefulWidget {
+  const Calendar2({Key? key}) : super(key: key);
+
+  @override
+  _Calendar2State createState() => _Calendar2State();
+}
+
+class _Calendar2State extends State<Calendar2> {
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+        focusedDay: DateTime.now(),
+        firstDay: DateTime(2022, 1, 1),
+        lastDay: DateTime(2023, 1, 31));
+  }
+}
